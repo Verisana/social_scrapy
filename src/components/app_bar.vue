f6e6be8d86e29903fac2a0c4fc9245942aff512d
Verisana <1902Grand@gmail.com>
2020-02-08 19:19:09 +0500

    Added vuetify and copied components

---
+++ src/components/app_bar.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app-bar app color="green darken-1" dark flat width="1024">
+    <v-app-bar-nav-icon @click="openHome">
+      <v-icon>home</v-icon>
+    </v-app-bar-nav-icon>
+    <v-toolbar-title>Project Title</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+    <v-btn icon @click="openSettings">
+      <v-icon>settings</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+
+export default {
+  name: 'app_bar',
+  data() {
+    return {};
+  },
+  methods: {
+    openHome() {
+      this.$router.push({ path: '/' });
+    },
+  },
+  mounted() {},
+  computed: {
+
+  },
+};
+</script>
+
+<style scoped lang="sass"></style>
---
+++ src/components/app_footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer color="indigo" app>
+    <span class="white--text text-center">&copy; 2020</span>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'app_footer',
+};
+</script>
+
+<style scoped></style>
